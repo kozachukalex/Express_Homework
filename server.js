128af68443573f468b286618448a744710ff6a13
KamiKozy <kamikozytpp@gmail.com>
2018-10-18 17:58:24 -0400

    atempt fix 2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require("body-parser");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
